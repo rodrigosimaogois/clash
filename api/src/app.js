58df057aa6f27c007e9762f29a6ce7dbc6768b50
Rodrigo <rodrigosimaogois@hotmail.com>
2026-08-08 12:14:09 +0200

    removed item

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import itemRoutes from './routes/item.routes.js';
 import configRouter from './routes/config.routes.js';
 import warRoutes from './routes/war.routes.js';
 
@@ -10,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 // /api for all routes
-app.use('/api', itemRoutes);
 app.use('/api', configRouter);
 app.use('/api', warRoutes);
 
