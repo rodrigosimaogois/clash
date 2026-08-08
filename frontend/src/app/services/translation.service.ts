39f14393b3482aaafb51881afacc7d766f9dbea3
Rodrigo <rodrigosimaogois@hotmail.com>
2026-08-08 12:01:50 +0200

    footer

--- frontend/src/app/services/translation.service.ts
+++ frontend/src/app/services/translation.service.ts
@@ -33,7 +33,8 @@ export class TranslationService {
       NO: 'Não',
       TITLE_MISSED_HISTORY: '3. Histórico de Ataques Perdedores / Não Realizados na Semana',
       TH_MISSED_DECKS: 'Decks Perderam nos Dias Anteriores',
-      NO_MISSED_MSG: 'Nenhum jogador perdeu ataques nos dias anteriores desta semana!'
+      NO_MISSED_MSG: 'Nenhum jogador perdeu ataques nos dias anteriores desta semana!',
+      FOOTER_TEXT: 'Desenvolvido com ❤️ por'
     },
     en: {
       SEARCH_TITLE: 'Clan War Lookup',
@@ -57,7 +58,8 @@ export class TranslationService {
       NO: 'No',
       TITLE_MISSED_HISTORY: '3. History of Missed / Lost Attacks This Week',
       TH_MISSED_DECKS: 'Decks Missed in Previous Days',
-      NO_MISSED_MSG: 'No players missed attacks in previous days this week!'
+      NO_MISSED_MSG: 'No players missed attacks in previous days this week!',
+      FOOTER_TEXT: 'Developed with ❤️ by'
     }
   };
 
